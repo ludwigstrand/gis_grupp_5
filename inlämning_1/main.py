493e12c53889904e8e4165ea2df520d7bf0ca066
ludwigstrand <ludwig.strand@live.com>
2024-04-26 18:40:51 +0200

    sdf

--- inlämning_1/main.py
+++ inlämning_1/main.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template      
+from flask import Flask, render_template, jsonify 
+from sklearn.cluster import KMeans
+import pandas as pd
 
 app = Flask(__name__)
+
+@app.route('/cluster')
+def cluster():
+
+    data = pd.read_csv('static/school_locations.csv')
+    
+    X = data[['xcoord', 'ycoord']].values 
+    k = 5  
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
+    data['cluster'] = kmeans.labels_  
+
+    centroids = kmeans.cluster_centers_.tolist()
+
+    response_data = {
+        'clusters': data.to_dict(orient='records'),
+        'centroids': centroids
+    }
+    return jsonify(response_data)
 
 @app.route("/")
 def home():
