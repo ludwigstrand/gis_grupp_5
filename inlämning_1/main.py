a90e6e64bd29a5f74b37263f527f6bac62d1c807
ludwigstrand <ludwig.strand@live.com>
2024-05-02 11:26:46 +0200

    färdig för inspelning

--- inlämning_1/main.py
+++ inlämning_1/main.py
@@ -25,10 +25,6 @@ def cluster():
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/iss")
-def iss():
-    return render_template("iss_spacestation.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
